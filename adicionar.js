d75fc37d3e9d5085db603ea5b0a861e8b2c80b7f
JohnKaick <john.kaick@gmail.com>
2016-10-12 21:26:22 -0300

    [ok]

---
+++ adicionar.js
@@ -0,0 +1,26 @@
+var botao = document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click", function(event){
+
+	event.preventDefault();
+
+	var campoNome = document.querySelector("#campo-nome");
+	var campoPeso = document.querySelector("#campo-peso");
+	var campoAltura = document.querySelector("#campo-altura");
+
+	var pacienteNovo = "<tr class='paciente'>"+
+						"<td class='info-nome'>"+ campoNome.value + "</td>"+
+						"<td class='info-peso'id='peso-1'>" + campoPeso.value + "</td>"+
+						"<td class='info-altura' id='altura-1'>"+ campoAltura.value + "</td>"+
+						"<td class='info-imc' id='imc-1'></td>"+
+					"</tr>";
+
+
+	var tabela = document.querySelector("table");
+	tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+
+	campoNome.value = "";
+	campoPeso.value = "";
+	campoAltura.value = "";
+
+})
